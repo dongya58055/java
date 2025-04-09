813245ea27993d7ada49906c50e5535a25441175
dong <dong58055@gmail.com>
2025-04-09 15:30:46 +0900

    第二次

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,9 +1,13 @@
 package test;
 
+import java.util.Iterator;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i+1);
+		}
 	}
 
 }
