e23d59571e7799322667d2d52a0912d93448d2c7
dong <dong58055@gmail.com>
2025-04-16 15:52:14 +0900

    随机数测试

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,13 +1,26 @@
 package test;
 
-import java.util.Iterator;
+import java.util.Random;
 
 public class Main {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 20; i++) {
-			System.out.println(i+1);
-		}
+//		for (int i = 0; i < 20; i++) {
+//			System.out.println(i+1);
+//		}
+		// System.out.println('a'+1);
+//		int a = 1, b = 2;
+//		System.out.println(a<b);
+//		int i=10,j=45,k=34;
+//		int max=i>j?i>k?i:k:j>k?j:k;
+//		System.out.println(max);
+//		Scanner sc = new Scanner(System.in);
+//		String name = sc.next("fd");
+//		System.out.println(name);
+		Random random = new Random();
+		Random r = new Random();
+		int i=r.nextInt(10);
+		System.out.println(i);
 	}
 
 }
